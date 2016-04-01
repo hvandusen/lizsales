bb74a6bd33378718649e15c6836d879b800674a3
henry van dusen <hgv2102@columbia.edu>
2016-04-01 01:06:03 -0400

    ascii and other stuff

--- page-home.php
+++ page-home.php
@@ -1,23 +1,49 @@
 <?php get_header(); ?>
 <div class='nav'>
-	<a class='left' href='#services'>services</a>
-	<a class='left'  href='#writers'>writers</a>
-	<a class='left' href='#process'>process</a>
-	<a class='right' href='/contact'>O---K</a>
-	<a class='right'  href='#pricing'>pricing</a>
+	<a class='left' href='#services'>Services</a>
+	<a class='left'  href='#writers'>Writers</a>
+	<a class='left' href='#process'>Process</a>
+	<a class='left'  href='#pricing'>Pricing</a>
+	<a class='right elementa' href='/form'>O---K</a>
+	<a class='right' href='#Contact'>Contact</a>
 </div>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 	 <?php //the_content(); ?>
+<div class='ascii typewriter'>
+	some centered ish
+</div>
 
-	<div class='ascii'>
-<pre>
-
- \       (_____________)       /
-	  \___________________________/
-</pre>
-	</div>
-
+<div class='ascii typewriter'>
+	<pre>
+	    ---------------------        
+	    |                   |        
+	    |     <span>I WRITE</span>       |        
+	    |     <span>ARTIST</span>        |        
+	    |     <span>STATEMENTS</span>    |        
+	(     |                   |&nbsp;&nbsp;        
+	  \  X=====U===========U======D        
+	   \___|_______  V ________|___        
+	    /      \  ///III\\\  /      \        
+	    /------- + ————————— + -------\        
+	    |    0 0 0 0 0 0 0 0 0 0 0 0    |        
+	    |     0 0 0 0 0 0 0 0 0 0 0     |        
+	    |    0 0 0 0 0 0 0 0 0 0 0 0    |        
+	    |     0 0 0 0 0 0 0 0 0 0 0     |        
+	    |    0 0 0 0 0 0 0 0 0 0 0 0    |        
+	    \       (_____________)       /        
+	    \___________________________/        
+	</pre>
+</div>
+<div class='ascii section'>
+<?php
+$headerBar = '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~(|––––––––––––|)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~';
+	$preName = '|(  ';
+	$postName = '  )|   ';
+	echo '<div>' . $headerBar . '</div>';
+	echo '<div>' . $preName . 'Services'. $postName . '</div>';
+	echo '<div>' . $headerBar . '</div>'; ?>
+</div>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
