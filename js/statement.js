cfd44c4a078519531ec3c371b3ed678d67e028ae
Henry Van Dusen <henry@dusendusen.com>
2019-02-08 11:27:47 -0500

    wow we crushed this fam

--- js/statement.js
+++ js/statement.js
@@ -1,76 +1,3 @@
-var verbs = ["abide","accelerate","accept","accomplish","achieve","acquire","acted","activate","adapt","add","address","administer","admire","admit","adopt","advise","afford","agree","alert","alight","allow","altered","amuse","analyze","announce","annoy","answer","anticipate","apologize","appear","applaud","applied","appoint","appraise","appreciate","approve","arbitrate","argue","arise","arrange","arrest","arrive","ascertain","ask","assemble","assess","assist","assure","attach","attack","attain","attempt","attend","attract","audited","avoid","awake","back","bake","balance","ban","bang","bare","bat","bathe","battle","be","beam","bear","beat","become","beg","begin","behave","behold","belong","bend","beset","bet","bid","bind","bite","bleach","bleed","bless","blind","blink","blot","blow","blush","boast","boil","bolt","bomb","book","bore","borrow","bounce","bow","box","brake","branch","break","breathe","breed","brief","bring","broadcast","bruise","brush","bubble","budget","build","bump","burn","burst","bury","bust","buy","buze","calculate","call","camp","care","carry","carve","cast","catalog","catch","cause","challenge","change","charge","chart","chase","cheat","check","cheer","chew","choke","choose","chop","claim","clap","clarify","classify","clean","clear","cling","clip","close","clothe","coach","coil","collect","color","comb","come","command","communicate","compare","compete","compile","complain","complete","compose","compute","conceive","concentrate","conceptualize","concern","conclude","conduct","confess","confront","confuse","connect","conserve","consider","consist","consolidate","construct","consult","contain","continue","contract","control","convert","coordinate","copy","correct","correlate","cost","cough","counsel","count","cover","crack","crash","crawl","create","creep","critique","cross","crush","cry","cure","curl","curve","cut","cycle","dam","damage","dance","dare","deal","decay","deceive","decide","decorate","define","delay","delegate","delight","deliver","demonstrate","depend","describe","desert","deserve","design","destroy","detail","detect","determine","develop","devise","diagnose","dig","direct","disagree","disappear","disapprove","disarm","discover","dislike","dispense","display","disprove","dissect","distribute","dive","divert","divide","do","double","doubt","draft","drag","drain","dramatize","draw","dream","dress","drink","drip","drive","drop","drown","drum","dry","dust","dwell","earn","eat","edited","educate","eliminate","embarrass","employ","empty","enacted","encourage","end","endure","enforce","engineer","enhance","enjoy","enlist","ensure","enter","entertain","escape","establish","estimate","evaluate","examine","exceed","excite","excuse","execute","exercise","exhibit","exist","expand","expect","expedite","experiment","explain","explode","express","extend","extract","face","facilitate","fade","fail","fancy","fasten","fax","fear","feed","feel","fence","fetch","fight","file","fill","film","finalize","finance","find","fire","fit","fix","flap","flash","flee","fling","float","flood","flow","flower","fly","fold","follow","fool","forbid","force","forecast","forego","foresee","foretell","forget","forgive","form","formulate","forsake","frame","freeze","frighten","fry","gather","gaze","generate","get","give","glow","glue","go","govern","grab","graduate","grate","grease","greet","grin","grind","grip","groan","grow","guarantee","guard","guess","guide","hammer","hand","handle","handwrite","hang","happen","harass","harm","hate","haunt","head","heal","heap","hear","heat","help","hide","hit","hold","hook","hop","hope","hover","hug","hum","hunt","hurry","hurt","hypothesize","identify","ignore","illustrate","imagine","implement","impress","improve","improvise","include","increase","induce","influence","inform","initiate","inject","injure","inlay","innovate","input","inspect","inspire","install","institute","instruct","insure","integrate","intend","intensify","interest","interfere","interlay","interpret","interrupt","interview","introduce","invent","inventory","investigate","invite","irritate","itch","jail","jam","jog","join","joke","judge","juggle","jump","justify","keep","kept","kick","kill","kiss","kneel","knit","knock","knot","know","label","land","last","laugh","launch","lay","lead","lean","leap","learn","leave","lecture","led","lend","let","level","license","lick","lie","lifted","light","lighten","like","list","listen","live","load","locate","lock","log","long","look","lose","love","maintain","make","man","manage","manipulate","manufacture","map","march","mark","market","marry","match","mate","matter","mean","measure","meddle","mediate","meet","melt","melt","memorize","mend","mentor","milk","mine","mislead","miss","misspell","mistake","misunderstand","mix","moan","model","modify","monitor","moor","motivate","mourn","move","mow","muddle","mug","multiply","murder","nail","name","navigate","need","negotiate","nest","nod","nominate","normalize","note","notice","number","obey","object","observe","obtain","occur","offend","offer","officiate","open","operate","order","organize","oriented","originate","overcome","overdo","overdraw","overflow","overhear","overtake","overthrow","owe","own","pack","paddle","paint","park","part","participate","pass","paste","pat","pause","pay","peck","pedal","peel","peep","perceive","perfect","perform","permit","persuade","phone","photograph","pick","pilot","pinch","pine","pinpoint","pioneer","place","plan","plant","play","plead","please","plug","point","poke","polish","pop","possess","post","pour","practice","praised","pray","preach","precede","predict","prefer","prepare","prescribe","present","preserve","preset","preside","press","pretend","prevent","prick","print","process","procure","produce","profess","program","progress","project","promise","promote","proofread","propose","protect","prove","provide","publicize","pull","pump","punch","puncture","punish","purchase","push","put","qualify","question","queue","quit","race","radiate","rain","raise","rank","rate","reach","read","realign","realize","reason","receive","recognize","recommend","reconcile","record","recruit","reduce","refer","reflect","refuse","regret","regulate","rehabilitate","reign","reinforce","reject","rejoice","relate","relax","release","rely","remain","remember","remind","remove","render","reorganize","repair","repeat","replace","reply","report","represent","reproduce","request","rescue","research","resolve","respond","restored","restructure","retire","retrieve","return","review","revise","rhyme","rid","ride","ring","rinse","rise","risk","rob","rock","roll","rot","rub","ruin","rule","run","rush","sack","sail","satisfy","save","saw","say","scare","scatter","schedule","scold","scorch","scrape","scratch","scream","screw","scribble","scrub","seal","search","secure","see","seek","select","sell","send","sense","separate","serve","service","set","settle","sew","shade","shake","shape","share","shave","shear","shed","shelter","shine","shiver","shock","shoe","shoot","shop","show","shrink","shrug","shut","sigh","sign","signal","simplify","sin","sing","sink","sip","sit","sketch","ski","skip","slap","slay","sleep","slide","sling","slink","slip","slit","slow","smash","smell","smile","smite","smoke","snatch","sneak","sneeze","sniff","snore","snow","soak","solve","soothe","soothsay","sort","sound","sow","spare","spark","sparkle","speak","specify","speed","spell","spend","spill","spin","spit","split","spoil","spot","spray","spread","spring","sprout","squash","squeak","squeal","squeeze","stain","stamp","stand","stare","start","stay","steal","steer","step","stick","stimulate","sting","stink","stir","stitch","stop","store","strap","streamline","strengthen","stretch","stride","strike","string","strip","strive","stroke","structure","study","stuff","sublet","subtract","succeed","suck","suffer","suggest","suit","summarize","supervise","supply","support","suppose","surprise","surround","suspect","suspend","swear","sweat","sweep","swell","swim","swing","switch","symbolize","synthesize","systemize","tabulate","take","talk","tame","tap","target","taste","teach","tear","tease","telephone","tell","tempt","terrify","test","thank","thaw","think","thrive","throw","thrust","tick","tickle","tie","time","tip","tire","touch","tour","tow","trace","trade","train","transcribe","transfer","transform","translate","transport","trap","travel","tread","treat","tremble","trick","trip","trot","trouble","troubleshoot","trust","try","tug","tumble","turn","tutor","twist","type","undergo","understand","undertake","undress","unfasten","unify","unite","unlock","unpack","untidy","update","upgrade","uphold","upset","use","utilize","vanish","verbalize","verify","vex","visit","wail","wait","wake","walk","wander","want","warm","warn","wash","waste","watch","water","wave","wear","weave","wed","weep","weigh","welcome","wend","wet","whine","whip","whirl","whisper","whistle","win","wind","wink","wipe","wish","withdraw","withhold","withstand","wobble","wonder","work","worry","wrap","wreck","wrestle","wriggle","wring","write","x-ray","yawn","yell","zip","zoom"]
-function statement(theFormData){
-  var pronouns = ['he','she','they'];
-  var possessives = ['his','hers','theirs'];
-  var possessive = "his";
-  var pronoun = "he";
-  var html = "<div class='statement'>";
-  var converters = {
-    " I ": " "+pronoun+" ",
-    "I ": pronoun+" ",
-    " am ":" is ",
-    "Because": "",
-    " my ": " "+possessive+" ",
-    "my ": possessive+" "
-  }
-  var media = localStorage["form-works-with"].split(", ");
-  for(var i in theFormData){
-    for(var j in converters){
-      if(theFormData[i].indexOf(j) > -1)
-        theFormData[i] = theFormData[i].replace(j,converters[j]);
-    }
-  }
-
-
-
-  html +=
-  theFormData["name"]+ " (b. "+
-  theFormData["bday"]+". "+
-  theFormData["birthplace"]+", "+
-  theFormData["country"]+")" +" is a "+
-  artistTitle(theFormData["works-with"])+ " who lives and works in "+
-  theFormData["work-city"]+". He works in "+(media.length>1 ? "these media":"this medium")+" because "+
-  ensurePeriod(theFormData["work-with"])+" He is inspired by the visual artist "+
-  theFormData["artists"]+" because "+
-  presentTense(theFormData["artists-2"]) + ". He is also inspired by the writer "+
-  theFormData["writers"]+
-  (theFormData["writers-2"].length>4 ? " because "+
-  lower(theFormData["writers-2"]): "")+
-  ". <br><br>The main subject of "+
-  theFormData["name"]+"'s work is "+
-  lower(theFormData["work-subject"])+" which is important to him because "+
-  theFormData["work-subject-2"] +". His art-making process consists of "+
-  ensurePeriod(lower(theFormData["process"]))+ " This is important to understanding his work because "+
-  lower(theFormData["process-2"])+". He connected to the contemporary critical dialog of his medium through the ideas of the theorist "+
-  theFormData["critical-dialogue"]+". The ideal exhibition space for his work is a "+
-  (theFormData["exh-space-2"].length>10 ?
-  lower(theFormData["exh-space"])+" because "+ensurePeriod(lower(theFormData["exh-space-2"])) :
-  lower(theFormData["exh-space"])+". "
-  )
-
-  html += "<br><br>";
-  // for(var i in theFormData){
-    // html+= "<span class='statement-text'>"+theFormData[i]+"</span>";//$("[name='"+(i.replace("form-",""))+"']").val(localStorage[i]);
-  // }
-  html+= "</div>";
-  return html;
-}
-
-function presentTense(text){
-  var words = text.split(" ");
-  words = words.map(function(e){
-    if(verbs.indexOf(e)> -1){
-      return e+"s";
-    }
-    return e;
-  })
-  return words.join(" ");
-}
-
-function removeI(text,pronoun){
-  return text.replace()
-}
-
 var worksWithConverter = {
   "Painting": "painter",
   "Photography": "photographer",
@@ -78,18 +5,30 @@ var worksWithConverter = {
   "Sculpture": "sculptor",
   "Performance": "performance artist",
   "Media art": "media artist",
-  "Installation art": "Installation",
+  "Installation art": "installation",
   "Film Video": "video artist",
   "Mixed media": "mixed media artist",
   "Bookarts": "book artist",
   "Text": "textual artist",
-  "I work in a variety of media": "works in a variety of media"
-}
+  "they work in a variety of media": "works in a variety of media"
+};
 
 function ensurePeriod(text){
-  if(text.slice(text.length-2).indexOf(".")>-1)
+  var regex = /[^\w\s]/g;
+  if(text.slice(text.length-2).search(regex)>-1){
     return text;
-  return text+"."
+  }
+  return text+".";
+}
+
+function stripPeriod(text){
+  if(text.slice(text.length-1).indexOf(".")>-1){
+    return text.slice(0,text.length-1);
+  }
+  if(text.slice(text.length-2).indexOf(".")>-1){
+    return text.slice(0,text.length-2);
+  }
+  return text;
 }
 
 function artistTitle(raw){
@@ -97,18 +36,19 @@ function artistTitle(raw){
   var types = raw.split(", ");
   types.map(function(e,i){
     // second to last
+    console.log("a type: ",e)
     if(i===types.length-2){
       output+= worksWithConverter[e]+", and ";
     }
     //last
     else if(i===types.length-1){
-      output+= worksWithConverter[e]
+      output+= worksWithConverter[e];
     }
     else {
         output+= worksWithConverter[e]+", "+ (i===types.length-2 ? "and " : "");
     }
 
-  })
+  });
   return output;
 }
 
@@ -117,50 +57,111 @@ function lower(string) {
     return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
+function statement(theFormData){
+  if(!theFormData){
+    return;
+  }
+  var html = "<div class='statement'>";
+  var converters = {
+    " my ": " their ",
+    "my ": "their ",
+    " My ": " their ",
+    "My ": "their ",
+    " our ": " their ",
+    "our ": "their ",
+    " Our ": " their ",
+    "Our ": "their ",
+    " I ": " they ",
+    "I ": "they ",
+    " we ": " they ",
+    "we ": "they ",
+    " We ": " they ",
+    "We ": "they ",
+    " me ": " them ",
+    "me ": "them ",
+    " am ":" are ",
+    "Because": ""
+  };
+//   " us ": " them ",
+// "us ": "them ",
+  var media = "";
+  if(theFormData){
+    media = theFormData.hasOwnProperty("works-with") ? theFormData["works-with"].split(", ") : "";
+  }
+  for(var i in theFormData){
+    for(var j in converters){
+      if(theFormData[i].indexOf(j) > -1){
+        theFormData[i] = theFormData[i].replace(j,converters[j]);
+      }
+    }
+  }
+  var lastName = theFormData["name"].split(" ").slice(-1)[0];
+  if(lastName.charAt(lastName.length-1) === "s" || lastName.charAt(lastName.length-1) === "z"){
+    lastName += "'";
+  }else{
+    lastName += "'s";
+  }
 
-var selector = '.outsput';
-var elements = document.querySelectorAll(selector);
-$z = 1;
+  html += (
+  theFormData["name"]+ printOrigin(theFormData) + " is a "+
+  artistTitle(theFormData["works-with"])+ " who lives and works in "+
+  theFormData["work-city"]+". "+
 
-// Bind functions to events
-for(var i = 0; i < elements.length; i++){
-  elements[i].addEventListener('mousedown', drag);
-  elements[i].addEventListener('mouseup', end);
-};
+  (theFormData["work-with"] ?
+    "They work in "+(media.length>1 ? "these media":"this medium")+" because "+ensurePeriod(lower(theFormData["work-with"])) :"") +
 
-// Drag function
-function drag(event) {
-  console.log("ok")
-  // Set variable to true on mousedown
-  $moving = true;
-  // Increase z-index so last clicked always on top
-  $z = $z+1;
-  // Select the item that was clicked
-  $this = event.target;
-  // Positions cursor in center of element when being dragged, as oposed to the top left
-  $width = $this.offsetWidth / 2;
-  $height = $this.offsetHeight / 2;
-  // Element follows mouse cursor
-  document.addEventListener('mousemove',function(e) {
-    // Only run if variable is true (this is destroyed on mouseup)
-    if($moving === true){
-      // Postion element, minus half width/height as above
-      var x = e.clientX - $width;
-      var y = e.clientY - $height;
+  (theFormData["artists"] ?
+    " They are inspired by the visual artist "+ stripPeriod(theFormData["artists"])+
+      (theFormData["artists-2"] ? " because "+ ensurePeriod(lower(theFormData["artists-2"]))
+      :".") :"")+
 
-      // Store left, top, and z-index in variable
-      var position = 'left:' + x + 'px;top:' + y + 'px;z-index:'+$z+';cursor:move;';
-      // Set style
-      $this.setAttribute('style', position);
-    };
-  });
-};
+  (theFormData["writers"] ?
+    "They are also inspired by the writer "+stripPeriod(theFormData["writers"])+
+    (theFormData["writers-2"].length>4 ? " because "+
+    lower(theFormData["writers-2"]): "")+
+    ". " :"")+
+  (theFormData["other-inspo"] ?
+    "They are also inspired by "+stripPeriod(lower(theFormData["other-inspo"]))+
+    (theFormData["other-inspo-2"].length>4 ? " because "+
+    ensurePeriod(lower(theFormData["other-inspo-2"])): ""):"")+
+  "<br><br>"+
+  (theFormData["work-subject"] ?
+    "The main subject of "+lastName+" work is "+
+    stripPeriod(lower(theFormData["work-subject"]))+
+      (theFormData["work-subject-2"] ?
+        " which is important to them because "+ ensurePeriod(lower(theFormData["work-subject-2"]))
+      :".") :"")+
+  (theFormData["process"] ?
+    " Their art-making process consists of "+
+    ensurePeriod(lower(theFormData["process"])):"")+
+  (theFormData["process-2"] ?
+    " This is important to understanding their work because "+
+  ensurePeriod(lower(theFormData["process-2"])) :"")+
+  (theFormData["critical-dialogue"] ?
+    " They connected to the contemporary critical dialog of their medium through the ideas of the theorist "+
+    ensurePeriod(theFormData["critical-dialogue"]) :"")+
+  (theFormData["exh-space-2"] ?
+  " The ideal exhibition space for their work is a "+
+  (theFormData["exh-space-2"].length>10 ?
+  stripPeriod(lower(theFormData["other-text"]))+" because "+ensurePeriod(lower(theFormData["exh-space-2"])) :
+  ensurePeriod(lower(theFormData["exh-space"]))) : "")
+);
 
-// Destroy drag on mouse up
-function end() {
-  console.log("ok")
-  $moving = false;
-};
+  html += "<br><br>";
+  html+= "</div>";
+  return html;
+}
+
+function printOrigin(theFormData){
+  var order = ["bday","birthplace","country"];
+  var stats = [];
+  order.map(function(e,i){
+    if(theFormData[e].length>2)
+      stats.push(e === "bday" ? "b. "+theFormData[e] : theFormData[e] );
+  })
+
+  return (stats.length > 0 ?" ("+ stats.join(", ") +")" : "");
+}
 
 
 window.statement = statement;
